ddc7e3461cc05d691a698c15e74a8c42aabf1676
Dennis Ferron <dennis.ferron@gmail.com>
2012-01-23 19:48:56 -0600

    Iocaste integrated with LikeMagic is working.

--- Interpreter/Source/Bindings.cpp
+++ Interpreter/Source/Bindings.cpp
@@ -7,11 +7,12 @@
 // (See the license file in LikeMagic/Licenses.)
 
 #include "Interpreter/Bindings.hpp"
+#include "LikeMagic/Backends/Io/IoVM.hpp"
 
-#include "Bindings/Irrlicht/Bindings.hpp"
-#include "Bindings/Bullet/Bindings.hpp"
-#include "Bindings/Custom/Bindings.hpp"
-#include "Bindings/DESteer/Bindings.hpp"
+//#include "Bindings/Irrlicht/Bindings.hpp"
+//#include "Bindings/Bullet/Bindings.hpp"
+//#include "Bindings/Custom/Bindings.hpp"
+//#include "Bindings/DESteer/Bindings.hpp"
 
 #ifdef USE_IRRKLANG
 #include "Bindings/IrrKlang/Bindings.hpp"
@@ -33,17 +34,17 @@ void add_bindings(LikeMagic::RuntimeTypeSystem& type_sys)
 {
     auto ns_bindings = Namespace::global(type_sys).subspace("Bindings");
 
-    auto ns_irrlicht = ns_bindings.subspace("Irrlicht");
-    LM_STATIC_FUNC(ns_irrlicht, Bindings::Irrlicht, add_bindings)
+    //auto ns_irrlicht = ns_bindings.subspace("Irrlicht");
+    //LM_STATIC_FUNC(ns_irrlicht, Bindings::Irrlicht, add_bindings)
 
-    auto ns_bullet = ns_bindings.subspace("Bullet");
-    LM_STATIC_FUNC(ns_bullet, Bindings::Bullet, add_bindings)
+    //auto ns_bullet = ns_bindings.subspace("Bullet");
+    //LM_STATIC_FUNC(ns_bullet, Bindings::Bullet, add_bindings)
 
-    auto ns_custom = ns_bindings.subspace("Custom");
-    LM_STATIC_FUNC(ns_custom, Bindings::Custom, add_bindings)
+    //auto ns_custom = ns_bindings.subspace("Custom");
+    //LM_STATIC_FUNC(ns_custom, Bindings::Custom, add_bindings)
 
-    auto ns_desteer = ns_bindings.subspace("DESteer");
-    LM_STATIC_FUNC(ns_desteer, Bindings::DESteer, add_bindings)
+    //auto ns_desteer = ns_bindings.subspace("DESteer");
+    //LM_STATIC_FUNC(ns_desteer, Bindings::DESteer, add_bindings)
 
 #ifdef USE_IRRKLANG
     auto ns_irrklang = ns_bindings.subspace("IrrKlang");
