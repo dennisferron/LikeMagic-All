e3a8d44347df4d23e2ee3216b4b87d5b5182c987
Dennis Ferron <dennis@dennis-desktop-linux.(none)>
2012-12-16 21:31:07 -0600

    Saving linux fixes.

---
+++ likemagic-setup.sh
@@ -0,0 +1,62 @@
+cd Downloads/
+ls
+chmod +x NVIDIA-Linux-x86_64-304.60.run 
+sudo ./NVIDIA-Linux-x86_64-304.60.run 
+sudo stop lightdm
+sudo stop gdm
+sudo service stop mdm
+sudo service mdm stop
+cd Downloads/
+sudo ./NVIDIA-Linux-x86_64-304.60.run 
+sudo shutdown now -h
+sudo apt-get install gcc g++ git codeblocks cmake cmake-qt-gui build-essential
+sudo apt-get install freeglut3-dev libxxf86vm-dev
+locate dl
+locate libdl
+nm /usr/lib/x86_64-linux-gnu/libdl.a 
+ls .ssh
+ssh-keygen -t rsa -C "system.windows.codeslinger@gmail.com"
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub 
+ssh -T git@github.com
+ls
+cd code
+ls
+git clone git@github.com:dennisferron/LikeMagic-All.git
+cd LikeMagic-All/
+git submodule init
+git submodule update
+git fetch origin dev
+git checkout dev
+git submodule foreach 'git fetch origin dev || true'
+git submodule foreach 'git checkout dev || true'
+cd ..
+git clone git@github.com:DropechoStudios/wayup.git
+cd wayup/
+git fetch origin dev
+git checkout dev
+cd ..
+gcc -v
+sudo apt-get install wine
+inxi -r
+ping archive.ubuntu.com
+ping 91.189.92.177
+sudo apt-get update
+sudo apt-get --fix-missing
+sudo apt-get update --fix-missing
+sudo apt-get install wine
+wine
+cd ..
+cd ~/.wine/dosdevices/c\:/Program\ Files\ \(x86\)/MegaSquirt/MS\ Download\ 2.00/
+ls
+wine gms2dl.exe 
+cp ~/Downloads/mfc42.dll ../../../windows/system32/
+wine gms2dl.exe 
+cp ~/Downloads/mfc42.dll ./
+ls
+wine gms2dl.exe 
+sudo apt-get uninstall banshee
+apt-cache search banshee
+sudo apt-get remove banshee
+sudo gsettings set org.gnome.desktop.media-handling automount false
+wine gms2dl.exe 
